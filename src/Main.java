b7a0f09c25e4cb2d8622a51c866a1dbd800f5ab7
miike413 <shpero1337@gmail.com>
2021-03-14 17:02:32 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,87 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("siema");
-    }
+
+        // Sklep gdzie musimy zrobić zakupy. Trzeba wybrac 5 produktów by wyjść ze sklepu. Jak weźmiemy produkt to ma on zniknąć z pocji
+        String[] klasa = {"Złodziej", "dziadek", "babcia"};
+        // produkty w sklepie
+        String[] shop = {"Bananas", "Orange", "Bread", "Yogurt", "CornFlakes", "Chicken"};
+        // koszyk o pojemności 5
+        String[] basket = new String[5];
+
+        Scanner scanner = new Scanner(System.in);
+//test
+
+   //test
+
+
+        boolean fullBasket = false;
+        int numberOfItemsInBasket = 0;
+        //klasa postaci
+
+        {
+        try {
+            System.out.println("\n Przed wejsciem do sklepu wybierz swoją postać od 0 -3 : ");
+            for (int i = 0; i < klasa.length; i++) {
+                System.out.println("[" + i + "] = " + klasa[i]);
+            }
+            int yourklasa = scanner.nextInt();
+            {System.out.println("Wybrałes postać:"+ yourklasa);}
+        }
+        catch(InputMismatchException inputMismatchException)
+            {
+                System.out.println("zły typ danych");
+
+            }
+        }
+
+
+        do
+            {
+            try {
+                System.out.println("\n Wybierz produkt ze sklepu : ");
+                for (int i = 0; i < shop.length; i++)
+                {
+                    System.out.println("[" + i + "] = " + shop[i]);
+                }
+                int yourChoice = scanner.nextInt();
+
+
+                if (yourChoice <= shop.length && yourChoice >= 0)
+                {
+
+                    if (!shop[yourChoice].equals("empty"))
+                    {
+                        basket[numberOfItemsInBasket] = shop[yourChoice];
+                        shop[yourChoice] = "empty";
+                        numberOfItemsInBasket++;
+                    }
+                    else
+                        {
+                        System.out.println("Nie ma takiego produktu");
+                        }
+
+                }
+                else
+                    {
+                    System.out.println("Nie ma takiej opcji w sklepie");
+                    }
+
+                if (numberOfItemsInBasket == 5) fullBasket = true;
+                System.out.println("Twój koszyk:");
+                for (int i = 0; i < numberOfItemsInBasket; i++) System.out.println("[" + i + "] = " + basket[i]);
+            }catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("zły typ danych");
+                scanner.next();
+            }
+        }
+        while (!fullBasket);
+        //while and case can we use here?
+        System.out.println("Wyszedłeś ze sklepu");
+
+        }
+
 }
